139274db8f4083c052e5bdb501f279d02b66b361
RainMaker3363 <jsc22637@gmail.com>
2017-03-12 21:44:24 +0900

    HY Test

--- Assets/Script/System/GameManager.cs
+++ Assets/Script/System/GameManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    private int TestInt;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        TestInt = 999;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
