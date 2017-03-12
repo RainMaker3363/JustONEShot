802a1b576c5dfececf83ced8bc690e86c7e33128
RainMaker3363 <jsc22637@gmail.com>
2017-03-12 18:26:38 +0900

    Week 1

---
+++ Assets/Script/Library/FrameChecker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameChecker : MonoBehaviour {
+
+    // 프레임 고정을 위한 변수
+    private int FrameRate;
+    private float deltaTime = 0.0f;
+
+    void Awake()
+    {
+        FrameRate = 60;
+
+        Application.targetFrameRate = 60;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+	}
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = new Color(0.0f, 0.0f, 1.5f, 1.5f);
+
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+
+        GUI.Label(rect, text, style);
+    }
+}
---
+++ Assets/Script/Library/TestGPGS.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.SavedGame;
+using System;
+using GooglePlayGames.BasicApi;
+
+public class TestGPGS : MonoBehaviour {
+
+
+    /// 현재 로그인 중인지 체크
+    public bool bLogin
+    {
+        get;
+        set;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        bLogin = false;
+
+        PlayGamesPlatform.Activate();
+
+        // 로그인이 안되어 있으면
+        if (!Social.localUser.authenticated)
+        {
+            Social.localUser.Authenticate(LoginCallBackGPGS);
+        }
+            
+
+    }
+
+    /// GPGS Login Callback
+    public void LoginCallBackGPGS(bool result)
+    {
+
+        bLogin = result;
+
+    }
+
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
