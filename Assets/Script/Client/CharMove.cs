8e8e7d7c7fac9f2d2d3309d10198a3d49c848a6e
LimSuDong <limsdong@naver.com>
2017-03-17 05:24:17 +0900

    Sync #2
    - 조이스틱 및 이동 구현
    - 이동, 조준, 발사 애니메이션 적용
    
    * 서로 상대 소스코드 구버전이 있을경우 잘못하면 구버전으로 업데이트 될 수 있으니 서로 주의해야할듯 합니다

---
+++ Assets/Script/Client/CharMove.cs
@@ -0,0 +1,383 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSD
+{
+    //플레이어 상태
+    enum PlayerState
+    {
+        IDLE,
+        DASH_SLOW,
+        DASH_SOFT,
+        DASH_HARD,
+        SHOT_READY,
+        SHOT_FIRE,
+        DAMAGE,
+        DEADEYE,
+        REROAD
+
+    }
+}
+
+
+public class CharMove : MonoBehaviour {
+
+   
+    public MoveJoyStick m_MoveJoyStickControl;  //움직임 전용 조이스틱
+    public JoyStickCtrl m_ShotJoyStickControl;  //샷 전용 조이스틱
+
+    public Animator anim;
+
+    public Transform m_CharPos;
+
+    public Camera cam;
+    private Vector3 CamPos;
+
+    // 플레이어의 움직임
+    private float m_MoveSpeed;
+    private Vector3 m_MoveVector;
+    private Vector3 m_PlayerDir;
+    //private Vector3 PlayerDeadEyePosBack;
+    private Vector3 m_PlayerPosBack;
+    public Transform m_GroundCheck;
+
+    LSD.PlayerState m_PlayerState;
+
+    //스테미나가 전부 소모된 상태
+    bool m_Exhausted = false;
+
+    // Use this for initialization
+    void Start () {
+
+        //카메라 기본위치 설정
+        CamPos = cam.transform.position;
+
+        // 플레이어 이동 방향 초기화
+        m_MoveVector = Vector3.zero;
+        m_MoveSpeed = 2.0f;
+        m_PlayerDir = Vector3.zero;
+        m_PlayerPosBack = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Debug.Log("VectorForce: " + m_MoveJoyStickControl.GetVectorForce());
+        Debug.Log("PlayerState: " + m_PlayerState);
+        
+        if(m_ShotJoyStickControl.GetTouch())
+        {
+            //    if(m_PlayerState != LSD.PlayerState.SHOT_FIRE)
+            m_PlayerState = LSD.PlayerState.SHOT_READY;
+        }
+
+        switch (m_PlayerState)
+        {
+            case LSD.PlayerState.IDLE:
+                {
+                    anim.SetInteger("DashLevel", 0);
+                    Update_IDLE();
+                    break;
+                }
+            case LSD.PlayerState.DASH_SLOW:
+                {
+                    anim.SetInteger("DashLevel", 1);
+                    Update_DASH_SLOW();
+                    break;
+                }
+            case LSD.PlayerState.DASH_SOFT:
+                {
+                    anim.SetInteger("DashLevel", 2);
+                    Update_DASH_SOFT();
+                    break;
+                }
+            case LSD.PlayerState.DASH_HARD:
+                {
+                    anim.SetInteger("DashLevel", 3);
+                    Update_DASH_HARD();
+                    break;
+                }
+            case LSD.PlayerState.SHOT_READY:
+                {
+                    anim.Play("Shot_Ready");
+                    anim.SetBool("ShotReady", true);
+                    Update_SHOT_READY();
+                    break;
+                }
+            case LSD.PlayerState.SHOT_FIRE:
+                {
+                    anim.SetBool("ShotReady", false);
+                    Update_SHOT_FIRE();
+                    break;
+                }
+            //case LSD.PlayerState.DAMAGE:
+            //    {
+            //        Update_DAMAGE();
+            //        break;
+            //    }
+            //case LSD.PlayerState.DEADEYE:
+            //    {
+            //        Update_DEADEYE();
+            //        break;
+            //    }
+            //case LSD.PlayerState.REROAD:
+            //    {
+            //        Update_REROAD();
+            //        break;
+            //    }
+
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+
+    void Update_IDLE()
+    {
+
+        if (!m_Exhausted)//탈진상태가 아니라면
+        {
+            if (m_MoveJoyStickControl.GetVectorForce() > 0)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_SOFT;
+            }
+            //else if (m_MoveJoyStickControl.GetVectorForce() > 0.5f)
+            //{
+            //    m_PlayerState = LSD.PlayerState.DASH_HARD;
+            //}
+        }
+        else //탈진상태라면
+        {
+            if (m_MoveJoyStickControl.GetVectorForce() > 0)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_SLOW;
+            }
+        }
+       
+
+    }
+
+    void Update_DASH_SLOW()
+    {
+        if (m_MoveJoyStickControl.GetVectorForce() > 0) // 계속 조작중인경우
+        {
+
+            if (!m_Exhausted) //탈진상태에서 회복됬다면
+            {
+                m_PlayerState++;    //Player상태를 DASH_SOFT로 변경
+                                    //m_PlayerState = LSD.PlayerState.DASH_SOFT;
+            }          
+        }
+        else //조작을 멈출경우
+        {
+            m_PlayerState--;//Player상태를 IDLE로 변경
+                            //m_PlayerState = LSD.PlayerState.IDLE;
+        }
+    }
+
+    void Update_DASH_SOFT()
+    {
+        if (!m_Exhausted)//탈진상태가 아니라면
+        {
+           
+            if (m_MoveJoyStickControl.GetVectorForce() > 0.5f)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_HARD;
+            }
+            else if (m_MoveJoyStickControl.GetVectorForce() > 0)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_SOFT;
+            }
+            else //조작을 멈출경우
+            {
+                m_PlayerState = LSD.PlayerState.IDLE;
+            }
+
+        }
+        else //탈진상태라면
+        {
+            if (m_MoveJoyStickControl.GetVectorForce() > 0)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_SLOW;
+            }
+        }
+    }
+
+    void Update_DASH_HARD()
+    {
+        if (!m_Exhausted)//탈진상태가 아니라면
+        {
+            
+            if (m_MoveJoyStickControl.GetVectorForce() > 0.5f)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_HARD;
+            }
+            else if(m_MoveJoyStickControl.GetVectorForce() > 0)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_SOFT;
+            }
+            else //조작을 멈출경우
+            {
+                m_PlayerState = LSD.PlayerState.IDLE;
+            }
+        }
+        else //탈진상태라면
+        {
+            if (m_MoveJoyStickControl.GetVectorForce() > 0)
+            {
+                m_PlayerState = LSD.PlayerState.DASH_SLOW;
+            }
+        }
+    }
+
+    void Update_SHOT_READY()
+    {
+        //조준중에 손을 땠다면
+        if(!m_ShotJoyStickControl.GetTouch())
+        {
+            m_PlayerState = LSD.PlayerState.SHOT_FIRE;
+            
+        }
+    }
+
+    void Update_SHOT_FIRE()
+    {
+        m_PlayerState = LSD.PlayerState.IDLE;
+    }
+
+    //void Update_DAMAGE()
+    //{
+
+    //}
+
+    //void Update_DEADEYE()
+    //{
+
+    //}
+
+    //void Update_REROAD()
+    //{
+
+    //}
+
+
+
+
+    void FixedUpdate()
+    {
+        switch (m_PlayerState)
+        {
+            case LSD.PlayerState.IDLE:
+                {
+                    FixedUpdate_IDLE();
+                    break;
+                }
+            case LSD.PlayerState.DASH_SLOW:
+                {
+                    FixedUpdate_DASH_SLOW();
+                    break;
+                }
+            case LSD.PlayerState.DASH_SOFT:
+                {
+                    FixedUpdate_DASH_SOFT();
+                    break;
+                }
+            case LSD.PlayerState.DASH_HARD:
+                {
+                    FixedUpdate_DASH_HARD();
+                    break;
+                }
+            case LSD.PlayerState.SHOT_READY:
+                {
+                    FixedUpdate_SHOT_READY();
+                    break;
+                }
+            case LSD.PlayerState.SHOT_FIRE:
+                {
+                    FixedUpdate_SHOT_FIRE();
+                    break;
+                }
+            case LSD.PlayerState.DAMAGE:
+                {
+                    FixedUpdate_DAMAGE();
+                    break;
+                }
+            case LSD.PlayerState.DEADEYE:
+                {
+                    FixedUpdate_DEADEYE();
+                    break;
+                }
+            case LSD.PlayerState.REROAD:
+                {
+                    FixedUpdate_REROAD();
+                    break;
+                }
+
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+
+    void FixedUpdate_IDLE()
+    {
+
+    }
+    void FixedUpdate_DASH_SLOW()
+    {
+        PlayerMove();
+    }
+    void FixedUpdate_DASH_SOFT()
+    {
+        PlayerMove();
+    }
+    void FixedUpdate_DASH_HARD()
+    {
+        PlayerMove();
+    }
+    void FixedUpdate_SHOT_READY()
+    {
+        PlayerAiming();
+    }
+    void FixedUpdate_SHOT_FIRE()
+    {
+
+    }
+    void FixedUpdate_DAMAGE()
+    {
+    }
+    void FixedUpdate_DEADEYE()
+    {
+
+    }
+    void FixedUpdate_REROAD()
+    {
+
+    }
+
+    void PlayerMove()
+    {
+
+        transform.rotation = m_MoveJoyStickControl.GetRotateVector();
+        transform.Translate(Vector3.forward * m_MoveSpeed * (float)m_PlayerState * Time.deltaTime);
+
+        RaycastHit Ground;
+        if (Physics.Raycast(m_GroundCheck.position, Vector3.down, out Ground,1.5f))
+        {
+            Debug.Log("HitPosition : "+Ground.transform.position);
+            transform.position = new Vector3(transform.position.x, Ground.transform.position.y, transform.position.z);
+        }
+
+        cam.transform.position = CamPos + transform.position;
+    }
+
+    void PlayerAiming()
+    {
+        transform.rotation = m_ShotJoyStickControl.GetRotateVector();
+    }
+}
---
+++ Assets/Script/Client/MoveJoyStick.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveJoyStick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+
+    // 조이스틱의 기판
+    public Image Joystick_Pad;
+    // 조이스틱의 스틱(Stick)
+    public Image joystick_Stick;
+    // 조이스틱의 이동 벡터 값
+    private Vector3 inputVector;
+
+    
+    bool TouchBegin = true;
+    private Vector3 BeginPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        Joystick_Pad.gameObject.SetActive(false);
+
+        if (Joystick_Pad == null)
+        {
+            //Joystick_Pad = GetComponent<Image>();
+        }
+
+        if (joystick_Stick == null)
+        {
+            joystick_Stick = transform.GetChild(0).GetComponent<Image>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 터치가 드래그(Drag) 했을때 호출 되는 함수
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 Pos;
+
+        // 터치된 로컬 좌표값을 Pos에 할당하고 Joystick_Pad 직사각형의 sizedelta 값으로 나누어
+        // Pos.X는 0~1, Pos.Y는 0~1 사이의 값으로 만듭니다.
+        // Joystick_Stick을 기준으로 좌우로 움직였을때 Pos.X는 -1~1 사이, 상하라면 Pos.Y를 -1~1의 값으로 변환하기 위해
+        // Pos.x * 2 + 1, Pos.y * 2 - 1 처리를 합니다.
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(Joystick_Pad.rectTransform, ped.position, ped.pressEventCamera, out Pos))
+        {
+            Pos.x = (Pos.x / Joystick_Pad.rectTransform.sizeDelta.x);
+            Pos.y = (Pos.y / Joystick_Pad.rectTransform.sizeDelta.y);
+
+            //Debug.Log("Pos.x :"+Pos.x);
+            //Debug.Log("Pos.y :" + Pos.y);
+            inputVector = new Vector3((Pos.x * 2 ), (Pos.y * 2) , 0);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+            //Debug.Log("inputVector: " + inputVector);
+            // 조이스틱이 움직인다면..
+            // joystick_Stick의 이미지를 터치한 좌표값으로 움직여준다.
+            // joystick_Stick.rectTransform.anchoredPosition = new Vector3(Pos.x* Joystick_Pad.rectTransform.sizeDelta.x, Pos.y* Joystick_Pad.rectTransform.sizeDelta.y);
+            joystick_Stick.rectTransform.anchoredPosition = new Vector3(inputVector.x * (joystick_Stick.rectTransform.sizeDelta.x),
+                 inputVector.y * (joystick_Stick.rectTransform.sizeDelta.y));
+        }
+    }
+
+    // 터치를 하고 있을 대 발생하는 함수
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        if(TouchBegin)
+        {
+            TouchBegin = false;
+            Joystick_Pad.gameObject.SetActive(true);
+            BeginPos = ped.position;
+            Joystick_Pad.gameObject.transform.position = BeginPos;
+        }
+
+        OnDrag(ped);
+    }
+
+    // 터치에서 손을 땠을때 발생하는 함수
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector3.zero;
+        joystick_Stick.rectTransform.anchoredPosition = Vector3.zero;
+
+        BeginPos = Vector3.zero;
+        Joystick_Pad.gameObject.SetActive(false);
+        TouchBegin = true;
+    }
+
+    //Player 스크립트에서 inputVector의 길이를 받기위해 사용될 함수
+    public float GetVectorForce()
+    {       
+        return inputVector.magnitude;
+    }
+
+    public Quaternion GetRotateVector()
+    {
+       Vector3 vec =  new Vector3(inputVector.x, 0, inputVector.y);
+
+        return Quaternion.LookRotation(vec);
+    }
+
+    public void InitInputVector()
+    {
+        //inputVector = Vector3.zero;
+        //joystick_Stick.rectTransform.anchoredPosition = Vector2.zero;
+    }
+}
