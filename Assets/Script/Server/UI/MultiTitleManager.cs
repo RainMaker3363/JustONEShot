edd985e880205f356f87d3e125328a840837236b
RainMaker3363 <jsc22637@gmail.com>
2017-05-20 04:21:30 +0900

    Sync# 13-1
    - 화면 잠김 방지 추가
    - 화면 비율 강제 추가 (주석처리)

--- Assets/Script/Server/UI/MultiTitleManager.cs
+++ Assets/Script/Server/UI/MultiTitleManager.cs
@@ -25,6 +25,22 @@ public class MultiTitleManager : MonoBehaviour
 
         //ButtonChecker = false;
         //MultiStartChecker = false;
+
+        /* 
+        * 유니티 엔진 사용 시 입력을 하지 않으면 모바일 장치의 화면이 어두워지다가 잠기게 되는데,
+        * 그러면 플레이어는 잠김을 다시 풀어야 해서 불편합니다. 따라서 화면 잠금 방지 기능 추가는 필수적이고,
+        * Screen.sleepTimeout를 아래처럼 설정하면 그걸 할 수 있습니다. 
+        */
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        // 지정해 주면 고정비로 빌드가 되어 단말에서 지정 해상도로 출력이 된다.	
+        //Screen.SetResolution(1280, 720, true); // 1280 x 720 으로 조정
+
+        //Screen.SetResolution(1920, 1080, true); // 1920 x 1080 으로 조정
+
+        //Screen.SetResolution(Screen.width, (Screen.width / 2) * 3 ); // 2:3 비율로 개발시
+
+        //Screen.SetResolution(Screen.width, Screen.width * 16 / 9,  true); // 16:9 로 개발시
     }
 
     void Update()
