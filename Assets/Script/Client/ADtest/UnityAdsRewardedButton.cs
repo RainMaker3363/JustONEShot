e0745dce0fdc4f0fbcf0c340f0b3eca3900cc813
RainMaker3363 <jsc22637@gmail.com>
2018-01-02 23:35:12 +0900

    WD Sync #2

--- Assets/Script/Client/ADtest/UnityAdsRewardedButton.cs
+++ Assets/Script/Client/ADtest/UnityAdsRewardedButton.cs
@@ -4,38 +4,38 @@ using UnityEngine.Advertisements; // Using the Unity Ads namespace.
 
 public class UnityAdsRewardedButton : MonoBehaviour
 {
-    public string zoneId;
-    public int rewardQty = 250;
+    //public string zoneId;
+    //public int rewardQty = 250;
 
-    void OnGUI()
-    {
-        if (string.IsNullOrEmpty(zoneId)) zoneId = null;
+    //void OnGUI()
+    //{
+    //    if (string.IsNullOrEmpty(zoneId)) zoneId = null;
 
-        Rect buttonRect = new Rect(10, 10, 150, 50);
-        string buttonText = Advertisement.IsReady(zoneId) ? "Show Ad" : "Waiting…";
+    //    Rect buttonRect = new Rect(10, 10, 150, 50);
+    //    string buttonText = Advertisement.IsReady(zoneId) ? "Show Ad" : "Waiting…";
 
-        ShowOptions options = new ShowOptions();
-        options.resultCallback = HandleShowResult;
+    //    ShowOptions options = new ShowOptions();
+    //    options.resultCallback = HandleShowResult;
 
-        if (GUI.Button(buttonRect, buttonText))
-        {
-            Advertisement.Show(zoneId, options);
-        }
-    }
+    //    if (GUI.Button(buttonRect, buttonText))
+    //    {
+    //        Advertisement.Show(zoneId, options);
+    //    }
+    //}
 
-    private void HandleShowResult(ShowResult result)
-    {
-        switch (result)
-        {
-            case ShowResult.Finished:
-                Debug.Log("Video completed. User rewarded " + rewardQty + " credits.");
-                break;
-            case ShowResult.Skipped:
-                Debug.LogWarning("Video was skipped.");
-                break;
-            case ShowResult.Failed:
-                Debug.LogError("Video failed to show.");
-                break;
-        }
-    }
+    //private void HandleShowResult(ShowResult result)
+    //{
+    //    switch (result)
+    //    {
+    //        case ShowResult.Finished:
+    //            Debug.Log("Video completed. User rewarded " + rewardQty + " credits.");
+    //            break;
+    //        case ShowResult.Skipped:
+    //            Debug.LogWarning("Video was skipped.");
+    //            break;
+    //        case ShowResult.Failed:
+    //            Debug.LogError("Video failed to show.");
+    //            break;
+    //    }
+    //}
 }
